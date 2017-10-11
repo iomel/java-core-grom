019a6c64ce55c94f3b5d229e7a9d37f9721e7937
iomel <igor.omel@gmail.com>
2017-10-11 16:05:42 +0200

    les 15 API

--- src/lesson15/API/GoogleAPI.java
+++ src/lesson15/API/GoogleAPI.java
@@ -11,14 +11,14 @@ public class GoogleAPI implements API {
     public Room[] findRooms(int price, int persons, String city, String hotel) {
         int count = 0;
         for (Room r : rooms)
-            if(r.getPrice() == price && r.getPersons() == persons && r.getCityName() == city && r.getHotelName() == hotel)
+            if(r.getPrice() == price && r.getPersons() == persons && r.getCityName().equals(city) && r.getHotelName().equals(hotel))
                 count++;
 
         Room[] suitableRooms = new Room[count];
         count = 0;
 
         for (Room r : rooms)
-            if(r.getPrice() == price && r.getPersons() == persons && r.getCityName() == city && r.getHotelName() == hotel)
+            if(r.getPrice() == price && r.getPersons() == persons && r.getCityName().equals(city) && r.getHotelName().equals(hotel))
                 suitableRooms[count++] = r;
 
         return suitableRooms;
