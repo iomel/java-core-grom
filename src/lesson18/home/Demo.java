e6ecdb0d04bbf7fa5443bf9d9c74db0c1f4b45d9
iomel <igor.omel@gmail.com>
2017-10-14 21:49:29 +0200

    les 18 test 1

--- src/lesson18/home/Demo.java
+++ src/lesson18/home/Demo.java
@@ -1,4 +1,18 @@
 package lesson18.home;
 
+import java.util.Arrays;
+
 public class Demo {
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        System.out.println(Arrays.toString(solution.findNumbers("")));
+        System.out.println(Arrays.toString(solution.findNumbers(null)));
+        System.out.println(Arrays.toString(solution.findNumbers("    ")));
+        System.out.println(Arrays.toString(solution.findNumbers("   ddsf sdf dfhfh    dfg3fg 22")));
+        System.out.println(Arrays.toString(solution.findNumbers("   221 d   sdf 23 45464 4      /")));
+
+
+    }
 }
--- src/lesson18/home/Solution.java
+++ src/lesson18/home/Solution.java
@@ -1,4 +1,33 @@
 package lesson18.home;
 
 public class Solution {
+    public int[] findNumbers(String text)
+    {
+        if (text == null || text.isEmpty())
+            return null;
+
+        text = text.trim();
+        String[] words = text.split(" ");
+        int[] result = new int[words.length];
+        int count = 0;
+        for (String w : words)
+        {
+            w = w.trim();
+            if (w.isEmpty())
+                continue;
+            try {
+                result[count] = Integer.parseInt(w);
+                count++;
+            } catch (Exception e)
+            {
+                System.out.println("not a number");
+            }
+        }
+        if (count == 0)
+            return null;
+        int[] finalResult = new int[count];
+        for (int i =0; i < count; i++ )
+            finalResult[i] = result[i];
+        return finalResult;
+    }
 }
