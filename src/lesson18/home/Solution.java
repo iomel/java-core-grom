f7285e260f9ef8a1a9c533b461de3e008b236367
iomel <igor.omel@gmail.com>
2017-10-14 21:54:24 +0200

    les 18 test 2

--- src/lesson18/home/Solution.java
+++ src/lesson18/home/Solution.java
@@ -1,7 +1,48 @@
 package lesson18.home;
 
 public class Solution {
+
     public int[] findNumbers(String text)
+    {
+        if (text == null || text.isEmpty())
+            return null;
+
+        text = text.trim();
+        String[] words = text.split(" ");
+        int[] result = new int[words.length];
+        int count = 0;
+        for (String w : words)
+        {
+            w = w.trim();
+            if (w.isEmpty() || !checkDigit(w))
+                continue;
+                result[count] = Integer.parseInt(w);
+                count++;
+        }
+        if (count == 0)
+            return null;
+        int[] finalResult = new int[count];
+        for (int i =0; i < count; i++ )
+            finalResult[i] = result[i];
+        return finalResult;
+
+    }
+
+    private boolean checkDigit(String word)
+
+    {
+        for (char ch : word.toCharArray())
+            if (!Character.isDigit(ch))
+            {
+                System.out.println("not a number");
+                return false;
+            }
+        return true;
+    }
+
+
+
+/*    public int[] findNumbers(String text)
     {
         if (text == null || text.isEmpty())
             return null;
@@ -30,4 +71,6 @@ public class Solution {
             finalResult[i] = result[i];
         return finalResult;
     }
+
+*/
 }
