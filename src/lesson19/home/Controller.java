60191db3a39696d0f43cc42405fd17e7b2a14266
iomel <igor.omel@gmail.com>
2017-10-18 22:08:49 +0200

    les 19 HW add DEL new

--- src/lesson19/home/Controller.java
+++ src/lesson19/home/Controller.java
@@ -38,7 +38,7 @@ public class Controller {
         File[] files = storage.getFiles();
         for (int i = 0; i < files.length; i++)
         {
-            if ((files[i] == null || files[i].isEmpty()) && files[i].getId() == file.getId() ){
+            if ((files[i] != null || !files[i].isEmpty()) && files[i].getId() == file.getId() ){
                 files[i] = null;
                 break;
             }
