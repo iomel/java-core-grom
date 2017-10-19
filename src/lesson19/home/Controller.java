ce5de38fe6872c32bf682bc78defcdccc31acfa1
iomel <igor.omel@gmail.com>
2017-10-19 12:48:29 +0200

    les 19 HW PUT update

--- src/lesson19/home/Controller.java
+++ src/lesson19/home/Controller.java
@@ -14,12 +14,13 @@ public class Controller {
 
         File[] files = storage.getFiles();
         for (int i = 0; i < files.length; i++)
-            if (files[i] == null || files[i].isEmpty()){
+            if (files[i] == null){
                 files[i] = file;
-                break;
+                storage.setFiles(files);
+                return file;
             }
-        storage.setFiles(files);
-        return file;
+
+        return null;
     }
 
 
