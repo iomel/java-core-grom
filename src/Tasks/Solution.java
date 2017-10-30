18609a8974ba5614c5eccb088a5932e1a31dfcb9
iomel <igor.omel@gmail.com>
2017-10-30 21:45:56 +0100

    Task - Arrays comparison

--- src/Tasks/Solution.java
+++ src/Tasks/Solution.java
@@ -1,11 +1,19 @@
 package Tasks;
 
-import java.util.*;
-
 public class Solution {
     public boolean compareIntArrays(int[] first, int[] second) {
-        return compareArrays(wrap(first), wrap(second));
+        if (first == second)
+            return true;
+        if (first == null || second == null)
+            return false;
 
+        if (first.length != second.length)
+            return false;
+
+        for (int i=0; i < first.length; i++)
+            if(first[i] != second[i])
+                return false;
+        return true;
     }
     public boolean compareStringArrays(String[] first, String[] second){
         return compareArrays(first, second);
@@ -14,20 +22,16 @@ public class Solution {
     public <T> boolean compareArrays(T[] first, T[] second) {
         if (first == second)
             return true;
+        if (first == null || second == null)
+            return false;
+
         if (first.length != second.length)
             return false;
-        Arrays.sort(first);
-        Arrays.sort(second);
 
         for (int i=0; i < first.length; i++)
-            if (first[i] != second[i])
+            if(!(first[i] == null ? second[i]==null : first[i].equals(second[i])))
                 return false;
         return true;
-    }
-    private Integer[] wrap(int[] array){
-        Integer[] result = new Integer[array.length];
-        for(int i=0; i < array.length; i++)
-            result[i] = array[i];
-        return result;
+
     }
 }
