22cb955f5f2ea401bdd1033f5991b58bcdb531c2
iomel <igor.omel@gmail.com>
2017-10-30 12:35:24 +0100

    Les 28 Comparators add null item check

--- src/lesson28/owncompare/FullComparator.java
+++ src/lesson28/owncompare/FullComparator.java
@@ -9,43 +9,23 @@ public class FullComparator implements Comparator<Capability>{
         if(o1 == o2 && o1 == null)
             return 0;
 
-        if (nameCheck(o1, o2) != 0)
-            return nameCheck(o1, o2);
-
-        if (fingerprintCheck(o1, o2) !=0)
-            return fingerprintCheck(o1, o2);
-
-       return dateCheck(o1, o2);
-    }
-
-    private int  fingerprintCheck(Capability o1, Capability o2){
-        if (o1.getFingerprint() == null)
-            return 1;
-        else if (o2.getFingerprint() == null)
-            return -1;
-        else
-            return o1.getFingerprint().compareTo(o2.getFingerprint());
-    }
-
-    private int  dateCheck(Capability o1, Capability o2){
-        if (o1.getDateCreated() == null)
-            return 1;
-        else if (o2.getDateCreated() == null)
-            return -1;
-        else
-            return o1.getDateCreated().compareTo(o2.getDateCreated());
-    }
-
-    private int  nameCheck(Capability o1, Capability o2){
-        if (o1.getChannelName() == null)
-            return 1;
-        else if (o2.getChannelName() == null)
-            return -1;
-        else
+        if (o1.getChannelName() != null && o2.getChannelName() != null && !o1.getChannelName().equals(o2.getChannelName()))
             return o1.getChannelName().compareTo(o2.getChannelName());
+        else if ((o1.getChannelName() == null || o2.getChannelName() == null) && o1.getChannelName() != o2.getChannelName())
+            return o1.getChannelName() == null ? 1 : -1;
+
+        if (o1.getFingerprint() != null && o2.getFingerprint() != null && !o1.getFingerprint().equals(o2.getFingerprint()))
+            return o1.getFingerprint().compareTo(o2.getFingerprint());
+        else if ((o1.getFingerprint() == null || o2.getFingerprint() == null) && o1.getFingerprint() != o2.getFingerprint())
+            return o1.getFingerprint() == null ? 1 : -1;
+
+        if (o1.getDateCreated() != null && o2.getDateCreated() != null)
+            return o1.getDateCreated().compareTo(o2.getDateCreated());
+        else if ((o1.getDateCreated() == null || o2.getDateCreated() == null) && o1.getDateCreated() != o2.getDateCreated())
+            return o1.getDateCreated() == null ? 1 : -1;
+
+        return 0;
     }
-
-
 
 
 }
