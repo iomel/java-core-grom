264484446723437080905c92d3b00a906cf09676
iomel <igor.omel@gmail.com>
2017-10-30 21:05:15 +0100

    Task - Arrays comparison

---
+++ src/Tasks/ArrayDemo.java
@@ -0,0 +1,21 @@
+package Tasks;
+
+public class ArrayDemo {
+
+    public static void main(String[] args) {
+        String[] first = {"we", "sdf", "sda", "aaa"};
+        String[] second = {"wse", "1sdf", "sdaa", "2aaa"};
+        Solution solution = new Solution();
+
+        System.out.println(solution.compareStringArrays(first, second));
+
+        int[] firstInt = {1, 34, 0, 14};
+        int[] secondInt = {32,2,12,0,4};
+        System.out.println(solution.compareIntArrays(firstInt, secondInt));
+
+        int[] thirdInt = secondInt;
+        System.out.println(solution.compareIntArrays(thirdInt, secondInt));
+
+
+    }
+}
---
+++ src/Tasks/Solution.java
@@ -0,0 +1,33 @@
+package Tasks;
+
+import java.util.Arrays;
+
+public class Solution {
+    public boolean compareIntArrays(int[] first, int[] second) {
+        return compareArrays(wrap(first), wrap(second));
+
+    }
+    public boolean compareStringArrays(String[] first, String[] second){
+        return compareArrays(first, second);
+    }
+
+    public <T> boolean compareArrays(T[] first, T[] second) {
+        if (first == second)
+            return true;
+        if (first.length != second.length)
+            return false;
+        Arrays.sort(first);
+        Arrays.sort(second);
+
+        for (int i=0; i < first.length; i++)
+            if (first[i] != second[i])
+                return false;
+        return true;
+    }
+    private Integer[] wrap(int[] array){
+        Integer[] result = new Integer[array.length];
+        for(int i=0; i < array.length; i++)
+            result[i] = array[i];
+        return result;
+    }
+}
