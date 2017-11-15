1836387fcede6aa1de644cb5f1c1418c0c3108a5
iomel <igor.omel@gmail.com>
2017-11-15 21:37:24 +0100

    Les 33 read|write class

--- src/lesson33/ReadWriteFile.java
+++ src/lesson33/ReadWriteFile.java
@@ -6,12 +6,12 @@ import java.io.*;
 
 public class ReadWriteFile {
 
-    public void readFile(String path) throws FileNotFoundException {
+    public void readFile(String path){
         FileReader streamReader;
         try {
             streamReader = new FileReader(path);
         } catch (FileNotFoundException e){
-            System.out.println("File not exist!");
+            System.out.println("File with path " + path + " not found");
             return;
         }
         BufferedReader reader = new BufferedReader(streamReader);
@@ -22,7 +22,7 @@ public class ReadWriteFile {
                 System.out.println(line);
             }
         } catch(IOException e){
-            System.err.println("Reading from file " +path+ " failed");
+            System.err.println("Can't read file by path " + path);
         } finally {
             IOUtils.closeQuietly(reader);
             IOUtils.closeQuietly(streamReader);
@@ -35,13 +35,54 @@ public class ReadWriteFile {
         try {
             streamWriter = new FileWriter(path, true);
             writer = new BufferedWriter(streamWriter);
-            writer.append("\n" + content);
+            writer.append(content);
         } catch(IOException e){
-            System.err.println("Can't write to file");
+            System.err.println("Can't write to file with path " + path);
         } finally {
             IOUtils.closeQuietly(writer);
             IOUtils.closeQuietly(streamWriter);
         }
     }
 
+    public void writeToFileFromConsole(String path)
+    {
+        InputStreamReader console = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(console);
+        String textToWrite = "";
+        String input;
+
+        if (new File(path).exists()) {
+            System.out.println("Enter file content to write in the file:");
+
+            try {
+                while (!(input = reader.readLine()).equals("wr"))
+                    textToWrite = textToWrite.concat(input);
+            } catch (IOException e) {
+                System.out.println("Can't read from console!");
+            } finally {
+                IOUtils.closeQuietly(reader);
+                IOUtils.closeQuietly(console);
+            }
+            writeFile(path, textToWrite);
+        } else
+            System.out.println("File with path " + path + " not found");
+    }
+
+
+    public void readFileByConsolePath(){
+        InputStreamReader console = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(console);
+        String path = "";
+        System.out.println("Please, enter file path to read:");
+
+        try {
+            path = reader.readLine();
+        } catch (IOException e) {
+            System.out.println("Can't read from console!");
+        } finally {
+            IOUtils.closeQuietly(reader);
+            IOUtils.closeQuietly(console);
+        }
+        readFile(path);
+    }
 }
