dc6078c942cfeff7674fe10f29cec57f154825a4
iomel <igor.omel@gmail.com>
2017-11-15 14:49:49 +0100

    Les 32 Example

--- src/lesson32/home/Demo.java
+++ src/lesson32/home/Demo.java
@@ -10,18 +10,6 @@ public class Demo {
         System.out.println("Summ is: " + solution.readNumbers());
 
 
-        private boolean isWord (String word) {
-            if (word != null && word.length()>2 && !word.isEmpty() && word.split(" ").length == 1) {
-                for (char ch : word.toCharArray())
-                    if (!Character.isLetter(ch) || Character.isWhitespace(ch))
-                        return false;
-                return true;
-               }
-            return false;
-         }
-
-
-
-    }
+     }
 
 }
--- src/lesson32/keyboard/ReadFromKeyboard.java
+++ src/lesson32/keyboard/ReadFromKeyboard.java
@@ -7,45 +7,33 @@ public class ReadFromKeyboard {
 
     public void readKeyboardWithScanner(){
         Scanner scanner = new Scanner(System.in);
-        String name;
-        while (true) {
-            System.out.println("Please enter your name:");
-            name = scanner.nextLine();
-            if(isWord(name)){
-                System.out.println("Hello, " + name + "!");
-                break;
-            } else
-                System.out.println("Wrong word, try again!");
-        }
+        System.out.println("Please enter your name:");
+        String name = scanner.nextLine();
+        if(isWord(name))
+           System.out.println("Hello, " + name + "!");
+        else
+           System.out.println("Wrong word, try again!");
     }
 
     public void readKeyboardWithIOStream() throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        String name;
-        while (true) {
-            System.out.println("Please enter your name:");
-            name = reader.readLine();
-            if(isWord(name)){
-                System.out.println("Hello, " + name + "!");
-                break;
-            } else
-                System.out.println("Wrong word, try again!");
-        }
+        System.out.println("Please enter your name:");
+        String name = reader.readLine();
+        if(isWord(name))
+            System.out.println("Hello, " + name + "!");
+        else
+            System.out.println("Wrong word, try again!");
+
         reader.close();
     }
 
     private boolean isWord (String word) {
-        boolean isWord = true;
-
         if (word != null && word.length()>2) {
             for (char ch : word.toCharArray())
-                if (!Character.isLetter(ch)) {
-                    isWord = false;
-                    break;
-                }
-        } else
-            isWord = false;
-        return isWord;
+                if (!Character.isLetter(ch))
+                    return false;
+            return true;
+        }
+        return false;
     }
-
 }
