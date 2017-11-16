e1c8435382b35db17235903a73185ac9970cd433
iomel <igor.omel@gmail.com>
2017-11-16 13:24:34 +0100

    Les 33 separate classes

--- src/lesson33/Demo.java
+++ src/lesson33/Demo.java
@@ -1,12 +1,13 @@
 package lesson33;
 
-import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class Demo {
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args) throws IOException{
         ReadWriteFile rwf = new ReadWriteFile();
 
         rwf.readFile("E:\\Test\\source\\new1.txt");
         rwf.writeFile("E:\\Test\\source\\new1.txt", "sdfgsdfg sfgsdfg new!");
+
     }
 }
--- src/lesson33/ReadWriteFile.java
+++ src/lesson33/ReadWriteFile.java
@@ -43,5 +43,4 @@ public class ReadWriteFile {
             IOUtils.closeQuietly(streamWriter);
         }
     }
-
 }
