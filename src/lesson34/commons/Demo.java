35b3b71c13944cf5a823c67884c6b178408ca6cd
iomel <igor.omel@gmail.com>
2017-11-21 07:47:39 +0100

    Les 34 Utils copy

--- src/lesson34/commons/Demo.java
+++ src/lesson34/commons/Demo.java
@@ -1,4 +1,13 @@
 package lesson34.commons;
 
+
 public class Demo {
+
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+//        solution.copyFileContent("E:\\Test\\test\\new2.txt", "E:\\Test\\test\\result.txt");
+        solution.copyFileContentApacheIO("E:\\Test\\test\\result.txt", "E:\\Test\\test\\new2.txt");
+
+    }
+
 }
--- src/lesson34/commons/Solution.java
+++ src/lesson34/commons/Solution.java
@@ -3,29 +3,47 @@ package lesson34.commons;
 import org.apache.commons.io.FileUtils;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
 
 public class Solution {
-    public void copyFileContent(String fileFromPath, String fileToPath) {
 
-        File sourceFile = new File(fileFromPath);
-        File destinationFile = new File(fileToPath);
+    public void copyFileContent(String fileFromPath, String fileToPath) throws Exception {
 
-        if (sourceFile.exists() && sourceFile.canRead() && !destinationFile.exists())
+            validate(fileFromPath, fileToPath);
+
             try {
-                Files.copy(sourceFile.toPath(), destinationFile.toPath());
+                Files.copy(new File(fileFromPath).toPath(), new File(fileToPath).toPath(), StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
                 System.err.println(e.getMessage());
             }
     }
 
-    public void copyFileContentApacheIO(String fileFromPath, String fileToPath) {
+    public void copyFileContentApacheIO(String fileFromPath, String fileToPath) throws Exception {
+
+        validate(fileFromPath, fileToPath);
+
         try {
             FileUtils.writeLines(new File(fileToPath), FileUtils.readLines(new File(fileFromPath), "UTF-8"), true);
         } catch (IOException e){
             System.err.println(e.getMessage());
         }
+    }
+
+    private void validate(String fileFromPath, String fileToPath) throws Exception{
+        File fileFrom = new File(fileFromPath);
+        File fileTo = new File(fileToPath);
+
+        if (!fileFrom.exists())
+            throw new FileNotFoundException("File " + fileFrom + "does not exist");
+        if (!fileTo.exists())
+            throw new FileNotFoundException("File " + fileTo + "does not exist");
+        if (!fileFrom.canRead())
+            throw new IOException("Can't read file" + fileFrom);
+        if (!fileTo.canWrite())
+            throw new IOException("Can't write to file" + fileTo);
     }
 
 }
